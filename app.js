1d6f37e04b11857277160ebfcb5c62f511c3c9bc
Bilalshah0 <121691643+Bilalshah0@users.noreply.github.com>
2023-01-26 13:18:00 +0500

    Add files via upload
    Javascript Alert assignment

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// Javascrip Assignment 01
+
+
+//      *********Question no 01*********
+
+// alert("Hey there! Welcome to my Website");
+
+
+//      *********Question no 02*********
+
+// alert("Error! Please enter a valid Password");
+
+
+//      *********Question no 03*********
+
+// alert("Welcome to JS Land...  \nHappy coding! ");
+
+
+//      *********Question no 04*********
+
+// alert("Welcome to JS Land...   ");
+// alert("Happy Coding!");
+
+
+//      *********Question no 05********* 
+
+// var alert = ("Hello... I can run JS through my web broser's console");
+// console.log(alert);
+
+
+//      *********Question no 06*********
+
+
+
+
+
+
